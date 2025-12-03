3e1dc1fe7ed5e8656c0b621803574fc744d9ff6b
Joan Climent <joanclimentq@gmail.com>
2025-12-03 19:45:21 +0100

    Capa de servicio implementada

--- src/main/java/madstodolist/repository/TareaRepository.java
+++ src/main/java/madstodolist/repository/TareaRepository.java
@@ -3,5 +3,8 @@ package madstodolist.repository;
 import madstodolist.model.Tarea;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TareaRepository extends CrudRepository<Tarea, Long> {
+    List<Tarea> findByUsuarioId(Long idUsuario);
 }
