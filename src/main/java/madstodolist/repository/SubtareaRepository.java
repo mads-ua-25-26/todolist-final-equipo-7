e7e27104d8d7b14af34ef85998dc5f651ac1927c
Joan Climent <joanclimentq@gmail.com>
2025-12-11 14:04:10 +0100

    Cambiado el controlador y borradas las clases sobrantes, ya funciona con el nuevo enfoque

--- src/main/java/madstodolist/repository/SubtareaRepository.java
+++
@@ -1,7 +0,0 @@
-package madstodolist.repository;
-
-import madstodolist.model.Subtarea;
-import org.springframework.data.repository.CrudRepository;
-
-public interface SubtareaRepository extends CrudRepository<Subtarea, Long> {
-}
